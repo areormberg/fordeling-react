33fb9c352080f89ea82edd2eda3f780f21cbca40
unknown <are.ormberg@gmail.com>
2018-10-17 21:01:37 +0200

    Added package.json and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+//WriteFileWebpackPlugin = require('write-file-webpack-plugin');
+//path = require('path');
+module.exports = {
+
+  entry: [
+    'whatwg-fetch',
+    'promise-polyfill',
+    './src/index.js'
+
+  ],
+
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+
+  },
+ /* plugins: [
+    new WriteFileWebpackPlugin()
+  ],*/
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
